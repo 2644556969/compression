aa63dbf497b5734ea3259c0046be478def6dcc3e
Jason Chen of group feigenbaum <jhc78@grace1.grace.hpc.yale.internal>
2020-04-16 14:54:50 -0400

    new data

--- MNIST/runtests.sh
+++ MNIST/runtests.sh
@@ -10,6 +10,7 @@ FILENAME=$1
 for i in {1..10}
 do 
 	#echo "Print $i" > "./$DIRECTORY/$FILENAME$i"
-	python Compressnets/train.py --epochs=20 --batch_size=128 > "./$DIRECTORY/$FILENAME$i" & 
+	
+	python Compressnets/train.py --epochs=20 --batch_size=128  > "./$DIRECTORY/$FILENAME$i" & 
 done 
 
